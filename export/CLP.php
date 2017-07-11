e4e98c7915131a963db5325b02ca6de7ca16ba39
rosyid_ido <rosyid33@gmail.com>
2017-07-11 08:10:28 +0700

    export pdf

--- export/CLP.php
+++ export/CLP.php
@@ -2,29 +2,30 @@
 include_once "../database.php";
 include_once "../fungsi.php";
 include_once "fpdf16/fpdf.php";
-$id_process = $_REQUEST['id_process'];
+$IdProyek = $_REQUEST['proyekId'];
 //object database class
 $db_object = new database();
 
-
+$proyek_row = $db_object->find_in_table("proyek", "*", "WHERE id = " . $IdProyek);
 
 $sql_que = "SELECT
-            conf.*, log.start_date, log.end_date
-            FROM
-             confidence conf, process_log `log`
-            WHERE conf.id_process = '$id_process' "
-            . " AND conf.id_process = log.id "
-            . " AND conf.lolos=1 "
-            . " ORDER BY conf.from_itemset DESC";
+                hasil.*,
+                kontraktor.`nama_kontraktor`
+              FROM
+                hasil,
+                kontraktor
+              WHERE 
+              hasil.`kontraktor_id` = kontraktor.`id`
+              AND proyek_id = ".$IdProyek."
+              ORDER BY nilai_v DESC";
 
 $db_query = $db_object->db_query($sql_que) or die("Query gagal");
 //Variabel untuk iterasi
 $i = 0;
 //Mengambil nilai dari query database
 while ($data = $db_object->db_fetch_array($db_query)) {
-    $cell[$i][0] = price_format($data['confidence']);
-    $cell[$i][1] = "Jika konsumen membeli ".$data['kombinasi1']
-                    .", maka konsumen juga akan membeli ".$data['kombinasi2'] ;
+    $cell[$i][0] = $data['nama_kontraktor'];
+    $cell[$i][1] = price_format($data['nilai_v']);
     $i++;
 }
 
@@ -53,20 +54,20 @@ $pdf->Open();
 $pdf->AddPage();
 $pdf->SetFont('Times', 'B', 12);
 //Ln() = untuk pindah baris
-$pdf->Cell(28, 1, 'Laporan Hasil Analisa', 'LRTB', 0, 'C'); //TBLR (untuk garis)=> B = Bottom,
+$pdf->Cell(28, 1, 'Laporan Hasil Proses Topsis '.$proyek_row['nama_proyek'], 'LRTB', 0, 'C'); //TBLR (untuk garis)=> B = Bottom,
 $pdf->Ln();
 $pdf->Ln();
 
 $pdf->Cell(1, 1, 'No', 'LRTB', 0, 'C');
-$pdf->Cell(24, 1, 'Rule', 'LRTB', 0, 'C');
-$pdf->Cell(3, 1, 'Confidence', 'LRTB', 0, 'C');
+$pdf->Cell(24, 1, 'Kontraktor', 'LRTB', 0, 'C');
+$pdf->Cell(3, 1, 'Nilai', 'LRTB', 0, 'C');
 $pdf->Ln();
 $pdf->SetFont('Times', "", 10);
 for ($j = 0; $j < $i; $j++) {
     //menampilkan data dari hasil query database
     $pdf->Cell(1, 1, $j + 1, 'LBTR', 0, 'C');
-    $pdf->Cell(24, 1, $cell[$j][1], 'LBTR', 0, 'L');
-    $pdf->Cell(3, 1, $cell[$j][0], 'LBTR', 0, 'C');
+    $pdf->Cell(24, 1, $cell[$j][0], 'LBTR', 0, 'L');
+    $pdf->Cell(3, 1, $cell[$j][1], 'LBTR', 0, 'C');
     $pdf->Ln();
 }
 
